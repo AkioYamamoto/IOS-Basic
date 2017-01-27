b2727b0d15aab628ab8b5d9a3450bc5603f2aaa2
Akio Yamamoto <yamamoto.akio@yahoo.com>
2017-01-27 10:55:44 +0900

    Empty-project

---
+++ customclass/customclass/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  customclass
+//
+//  Created by Yamamoto on 1/27/17.
+//  Copyright © 2017 Yamamoto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
